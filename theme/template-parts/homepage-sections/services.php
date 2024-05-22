af4aede85acb253aa1f61debdd387f8745abb09a
Gio Victor Rodriguez <giorodriguez021@gmail.com>
2024-05-23 03:16:21 +0800

    Dynamic testimonials and services section

--- theme/template-parts/homepage-sections/services.php
+++ theme/template-parts/homepage-sections/services.php
@@ -1,7 +1,22 @@
 <?php
-/**
- * Template part for displaying the services section in homepage
- */
+	/**
+	 * Template part for displaying the services section in homepage
+	 */
+
+	$services = get_posts(array(
+		'posts_per_page' => -1,
+		'post_type'      => 'service',
+		'order'          => 'ASC'
+	));
+
+	$data = [];
+
+	foreach($services as $service) {
+		$data[] = [
+			'service'  => $service->post_title,
+			'bg_image' =>  get_the_post_thumbnail_url($service->ID)
+		];
+	}
 ?>
 
 <section id="services" class="h-screen py-16">
@@ -9,22 +24,18 @@
 		<h3 class="secondary-font text-center mb-16">Services</h3>
 
 		<div class="grid grid-cols-12 gap-1 items-center">
-			<div class="service col-span-4 mx-4 flex justify-center items-center secondary-font relative rounded-3xl bg-cover bg-no-repeat bg-center cursor-pointer">
-				<div class="overlay absolute w-full h-full top-0 left-0 rounded-3xl"></div>
-				<h4 class="text-center relative z-10 px-16">Customizable Gifts and Souvenirs</h4>
-			</div>
-
-			<div class="service col-span-4 mx-4 flex justify-center items-center secondary-font relative rounded-3xl bg-cover bg-no-repeat bg-center cursor-pointer">
-				<div class="overlay absolute w-full h-full top-0 left-0 rounded-3xl"></div>
-				<h4 class="text-center relative z-10 px-16">Event Styling</h4>
-			</div>
-
-			<div class="service col-span-4 mx-4 flex justify-center items-center secondary-font relative rounded-3xl bg-cover bg-no-repeat bg-center cursor-pointer">
-				<div class="overlay absolute w-full h-full top-0 left-0 rounded-3xl"></div>
-				<h4 class="text-center relative z-10 px-16">Rentals</h4>
-			</div>
+			<?php
+				foreach($data as $row) {
+			?>
+				<div class="service col-span-4 mx-4 flex justify-center items-center secondary-font relative rounded-3xl bg-cover bg-no-repeat bg-center cursor-pointer" style="background-image: url(<?php echo $row['bg_image'] ?>)">
+					<div class="overlay absolute w-full h-full top-0 left-0 rounded-3xl"></div>
+					<h4 class="text-center relative z-10 px-16"><?php echo $row['service']; ?></h4>
+				</div>
+			<?php
+				}
+			?>
 		</div>
-
+		
 		<button class="primary-button text-white rounded-full px-8 py-2 mt-12">Shop Now</button>
 	</div>
 </section>
--- theme/template-parts/homepage-sections/testimonials.php
+++ theme/template-parts/homepage-sections/testimonials.php
@@ -1,31 +1,41 @@
 <?php
-/**
- * Template part for displaying the testimonials section in homepage
- */
+	/**
+	 * Template part for displaying the testimonials section in homepage
+	 */
+
+	$testimonials = get_posts(array(
+		'posts_per_page' => -1,
+		'post_type'      => 'testimonial',
+		'order'          => 'ASC'
+	));
+
+	$data = [];
+
+	foreach($testimonials as $testimonial) {
+		$data[] = [
+			'customer'    => $testimonial->post_title,
+			'testimonial' => $testimonial->post_content,
+			'image'       => get_the_post_thumbnail_url($testimonial->ID)
+		];
+	}
 ?>
 
-<section id="testimonials" class="h-screen py-16">
+<section id="testimonials" class="h-screen py-24">
 	<div class="container mx-auto flex flex-col justify-center items-center">
-		<h3 class="secondary-font text-center mb-16">Client Testimonials</h3>
+		<h3 class="secondary-font text-center mb-24">Client Testimonials</h3>
 
 		<div class="grid grid-cols-12 gap-1 mt-20 items-center">
-			<div class="testimonial col-span-4 relative mx-4 px-12 pb-16 pt-40 flex flex-col justify-center items-center">
-				<h4>Christina Marie</h4>
-				<p class="font-light text-center">They are the best and most accommodating people to work with  Thank you Timeless Treasures!!! Guests loved the Mobile Beer Bins!</p>
-				<img class="absolute" src="<?php echo get_template_directory_uri() . "/assets/images/sample-testimonial-image.png"; ?>" />
-			</div>
-
-			<div class="testimonial col-span-4 relative mx-4 px-12 pb-16 pt-40 flex flex-col justify-center items-center">
-				<img class="absolute" src="<?php echo get_template_directory_uri() . "/assets/images/sample-testimonial-image.png"; ?>" />
-				<h4>Christina Marie</h4>
-				<p class="font-light text-center">They are the best and most accommodating people to work with  Thank you Timeless Treasures!!! Guests loved the Mobile Beer Bins!</p>
-			</div>
-
-			<div class="testimonial col-span-4 relative mx-4 px-12 pb-16 pt-40 flex flex-col justify-center items-center">
-				<img class="absolute" src="<?php echo get_template_directory_uri() . "/assets/images/sample-testimonial-image.png"; ?>" />
-				<h4>Christina Marie</h4>
-				<p class="font-light text-center">They are the best and most accommodating people to work with  Thank you Timeless Treasures!!! Guests loved the Mobile Beer Bins!</p>
-			</div>
+			<?php
+				foreach($data as $row) {
+			?>
+				<div class="testimonial col-span-4 relative mx-4 px-12 pb-16 pt-32">
+					<img class="absolute rounded-full top-0" src="<?php echo $row['image']; ?>" />
+					<h4 class="text-center mb-2"><?php echo $row['customer']; ?></h4>
+					<div class="font-light text-center overflow-hidden text-ellipsis"><?php echo $row['testimonial']; ?></div>
+				</div>
+			<?php
+				}
+			?>
 		</div>
 	</div>
 </section>
