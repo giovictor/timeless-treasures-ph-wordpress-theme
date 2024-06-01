23adb6c72c0de0e9fd5300286c8456f7173f88ae
Gio Victor Rodriguez <giorodriguez021@gmail.com>
2024-06-01 23:39:35 +0800

    Responsive footer

--- theme/template-parts/layout/footer-content.php
+++ theme/template-parts/layout/footer-content.php
@@ -9,14 +9,14 @@
 
 ?>
 
-<footer id="colophon" class="flex items-center py-20 lg:py-0">
+<footer id="colophon" class="flex items-center px-6 sm:px-0 py-8 lg:py-0">
 	<div class="container mx-auto">
-		<div class="grid grid-cols-12 gap-1 lg:items-center">
-			<div class="col-span-12 lg:col-span-4 mb-8 lg:mb-0 flex justify-center lg:justify-start">
-				<img src="<?php echo get_template_directory_uri() . "/assets/images/footer-logo.jpg"; ?>" />
+		<div class="grid grid-cols-12 gap-1 items-center">
+			<div class="col-span-12 lg:col-span-4 mb-0 lg:mb-8 flex justify-center">
+				<img src="<?php echo get_template_directory_uri() . "/assets/images/footer-logo.jpg"; ?>" class="footer-logo" />
 			</div>
 
-			<div class="col-span-12 md:col-span-6 lg:col-span-4">
+			<div class="col-span-6 lg:col-span-4 mb-8 flex justify-center">
 				<?php if ( has_nav_menu( 'menu-2' ) ) : ?>
 					<nav aria-label="<?php esc_attr_e( 'Footer Menu', 'timeless-treasures-ph' ); ?>">
 						<?php
@@ -32,28 +32,30 @@
 				<?php endif; ?>
 			</div>
 
-			<div class="col-span-12 md:col-span-6 lg:col-span-4 relative">
-				<span class="secondary-font mb-3 block">Follow us on:</span>
-				<div class="flex items-center mb-6">
-					<a href="#" target="_blank" class="mr-3">
-						<img src="<?php echo get_template_directory_uri() . "/assets/images/facebook-logo.png"; ?>" />
-					</a>
+			<div class="col-span-6 lg:col-span-4 mb-8 flex justify-center">
+				<div class="social-media-and-newsletter">
+					<span class="follow-us-on-label secondary-font mb-3 block">Follow us on:</span>
+					<div class="flex items-center mb-6">
+						<a href="#" target="_blank" class="mr-3">
+							<img src="<?php echo get_template_directory_uri() . "/assets/images/facebook-logo.png"; ?>" />
+						</a>
 
-					<a href="#" target="_blank" class="mr-3">
-						<img src="<?php echo get_template_directory_uri() . "/assets/images/instagram-logo.png"; ?>" />
-					</a>
+						<a href="#" target="_blank" class="mr-3">
+							<img src="<?php echo get_template_directory_uri() . "/assets/images/instagram-logo.png"; ?>" />
+						</a>
 
-					<a href="#" target="_blank" class="mr-3">
-						<img src="<?php echo get_template_directory_uri() . "/assets/images/tiktok-logo.png"; ?>" />
-					</a>
-				</div>
+						<a href="#" target="_blank" class="mr-3">
+							<img src="<?php echo get_template_directory_uri() . "/assets/images/tiktok-logo.png"; ?>" />
+						</a>
+					</div>
 
-				<span class="block mb-3">Newsletter</span>
-				<div class="relative w-72">
-					<input type="text" placeholder="Sign up with your email" class="newsletter-email py-2 pl-2 w-72 text-sm focus:outline-none" />
-					<button type="button" class="newsletter-email-button absolute text-white h-full right-0">
-						<img src="<?php echo get_template_directory_uri() . "/assets/images/newsletter-icon.png" ?>" class="mx-auto w-1/2" />
-					</button>
+					<span class="newsletter-label block mb-3">Newsletter</span>
+					<div class="relative">
+						<input type="text" placeholder="Email Address" class="newsletter-email py-2 pl-2 w-36 sm:w-40 md:w-72 text-xs sm:text-sm focus:outline-none" />
+						<button type="button" class="newsletter-email-button absolute text-white h-full right-0">
+							<img src="<?php echo get_template_directory_uri() . "/assets/images/newsletter-icon.png" ?>" class="mx-auto w-1/2" />
+						</button>
+					</div>
 				</div>
 			</div>
 		</div>
